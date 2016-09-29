d55858085ab016acae29418650b69ed769eb2908
7yon <simenkovadarya@gmail.com>
2016-09-29 15:06:44 +0500

    renamed methods CalculateArea(), changed methods IsValid..() and constructors

--- TheAreaOfATriangle/TheAreaOfATriangle/Triangle.cs
+++ TheAreaOfATriangle/TheAreaOfATriangle/Triangle.cs
@@ -18,69 +18,81 @@ namespace TheAreaOfATriangle
         public static Triangle FromThreeSides(double edgeA, double edgeB, double edgeC)
         {
             Triangle triangle = new Triangle();
-            triangle.IsValidThreeSides(edgeA, edgeB, edgeC);
+            if (triangle.IsValidThreeSides(edgeA, edgeB, edgeC))
+            {
+                triangle.edgeA = edgeA;
+                triangle.edgeB = edgeB;
+                triangle.edgeC = edgeC;
+            }
 
+            triangle.CalculateArea();
             return triangle;
         }
 
         public static Triangle FromTwoSidesAndAngle(double edgeA, double edgeB, double angle)
         {
             Triangle triangle = new Triangle();
-            triangle.IsValidTwoSidesAndAgle(edgeA, edgeB, angle);
+            if(triangle.IsValidTwoSidesAndAgle(edgeA, edgeB, angle))
+            {
+                double sideC = Math.Sqrt(Math.Pow(edgeA, 2) + Math.Pow(edgeB, 2) - 2 * edgeA * edgeB * Math.Cos(DegreeToRadian(agle)));
 
+                triangle.edgeA = edgeA;
+                triangle.edgeB = edgeB;
+                triangle.edgeC = sideC;
+            }
+
+            triangle.CalculateArea();
             return triangle;
         }
 
         public static Triangle FromTwoAglesAndSide(double angleA, double angleB, double edgeA)
         {
             Triangle triangle = new Triangle();
-            triangle.IsValidTwoAglesAndSide(angleA, angleB, edgeA);
+            if (triangle.IsValidTwoAglesAndSide(angleA, angleB, edgeA))
+            {
+                double angleS = 180 - angleA - angleB;
+                double dependenceSides = edgeA / Math.Sin(triangle.DegreeToRadian(angleS));
 
+                double sideB = Math.Sin(triangle.DegreeToRadian(angleB)) * dependenceSides;
+                double sideC = Math.Sin(triangle.DegreeToRadian(angleA)) * dependenceSides;
+
+                triangle.edgeA = edgeA;
+                triangle.edgeB = sideB;
+                triangle.edgeC = sideC;
+            }
+
+            triangle.CalculateArea();
             return triangle;
         }
 
-        private void IsValidThreeSides(double edgeA, double edgeB, double edgeC)
+        private bool IsValidThreeSides(double edgeA, double edgeB, double edgeC)
         {
             if ((edgeA + edgeB > edgeC) && (edgeA + edgeC > edgeB) && (edgeC + edgeB > edgeA))
             {
-                this.edgeA = edgeA;
-                this.edgeB = edgeB;
-                this.edgeC = edgeC;
+                return true;
             }
             else throw new ArgumentException("Не выполняется правило: Сумма двух любых сторон всегда больше третьей стороны\n");
         }
 
-        private void IsValidTwoSidesAndAgle(double edgeA, double edgeB, double agle)
+        private bool IsValidTwoSidesAndAgle(double edgeA, double edgeB, double agle)
         {            
             if ((agle > 0 && agle < 180) && (edgeA > 0) && (edgeB > 0))
             {
-                double sideC = Math.Sqrt(Math.Pow(edgeA, 2) + Math.Pow(edgeB, 2) - 2 * edgeA * edgeB * Math.Cos(DegreeToRadian(agle)));
-
-                this.edgeA = edgeA;
-                this.edgeB = edgeB;
-                this.edgeC = sideC;
+                return true;
             }        
             else throw new ArgumentException("Не выполняется одно или оба из правил: Стороны всегда принимают положительные значения и сумма двух углов всегда меньше 180 градусов\n");
         }
 
-        private void IsValidTwoAglesAndSide(double angleA, double angleB, double edgeA)
+        private bool IsValidTwoAglesAndSide(double angleA, double angleB, double edgeA)
         {
             if (angleA + angleB < 180)
             {
-                double angleS = 180 - angleA - angleB;
-                double dependenceSides = edgeA / Math.Sin(DegreeToRadian(angleS));
-
-                double sideB = Math.Sin(DegreeToRadian(angleB)) * dependenceSides;
-                double sideC = Math.Sin(DegreeToRadian(angleA)) * dependenceSides;
-
-                this.edgeA = edgeA;
-                this.edgeB = sideB;
-                this.edgeC = sideC;
+                return true;
             }
             else throw new ArgumentException("Не выполняется правило: Сумма двух углов меньше 180 градусов\n");
       }
 
-        public void CalculationArea()
+        private void CalculateArea()
         {
             double perimeter = (this.edgeA + this.edgeB + this.edgeC) / 2;           
             this.Area = Math.Sqrt(perimeter * (perimeter - edgeA) * (perimeter - edgeB) * (perimeter - edgeC));  
