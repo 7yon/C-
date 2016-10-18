291d0866722991dc9c70b5174878e37b6f3f5de3
7yon <simenkovadarya@gmail.com>
2016-10-18 23:16:18 +0500

    added new project - Processor Generic, part 1

---
+++ ProcessorGeneric/ProcessorGeneric/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessorGeneric
+{
+    public class Entity
+    {
+        Engine engine;
+
+        public Processor<Engine, Entity, Logger> With<TLogger>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
