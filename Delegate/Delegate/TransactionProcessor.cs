bcc80955adc25c8586825b08772d2154957d0353
7yon <simenkovadarya@gmail.com>
2016-10-27 11:05:04 +0500

    delegates part1

---
+++ Delegate/Delegate/TransactionProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class TransactionProcessor
+    {
+        private Func<TransactionRequest, bool> Check;
+        private Func<TransactionRequest, Transaction> Register;
+        private Action<Transaction> Save;
+
+        public TransactionProcessor(Func<TransactionRequest, bool> Check, Func<TransactionRequest, Transaction> Register, Action<Transaction> Save)
+        {
+            this.Check = Check;
+            this.Register = Register;
+            this.Save = Save;
+        }
+
+        public Transaction Process(TransactionRequest request)
+        {
+            if (!Check(request))
+                throw new ArgumentException();
+            var result = Register(request);
+            Save(result);
+            return result;
+        }
+    }
+}
