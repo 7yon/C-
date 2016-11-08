0a7bcf49e8b273a35c005fcdf7b18f30827ad8f2
7yon <simenkovadarya@gmail.com>
2016-11-08 16:17:52 +0500

    Memory management part 1

---
+++ Memory management/Memory management/MyTimer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Memory_management
+{
+    public class MyTimer : IDisposable
+    {
+        Stopwatch timer = new Stopwatch();
+        public long ElapsedMilliseconds
+        {
+            get
+            {
+                return timer.ElapsedMilliseconds;
+            }
+        }
+
+        public MyTimer()
+        {
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool fromDisposeMethod)
+        {
+            if (fromDisposeMethod)
+            {
+                timer.Stop();
+            }
+        }
+
+        public IDisposable Start()
+        {
+            timer.Reset();
+            timer.Start();
+
+            return this;
+        }
+
+        public IDisposable Continue()
+        {
+            timer.Start();
+
+            return this;
+        }
+    }
+}
---
+++ Memory management/Memory management/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Timers;
+
+namespace Memory_management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new MyTimer();
+            using (timer.Start())
+            {
+                for(int i = 0; i < 1000000; i++)
+                {
+
+                }
+            }
+            Console.WriteLine("after start " + timer.ElapsedMilliseconds);
+
+            using (timer.Continue())
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    for(int y = 0; y < 20000000; y++)
+                    {
+
+                    }
+                    Console.WriteLine("i - "+ i.ToString() + " " + timer.ElapsedMilliseconds);
+                }
+            }
+            Console.WriteLine("after continue " + timer.ElapsedMilliseconds);
+
+        }
+    }
+}
