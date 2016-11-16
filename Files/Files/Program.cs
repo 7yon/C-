206dd049818c39f866c55809e4a8ab8be5711ef1
7yon <simenkovadarya@gmail.com>
2016-11-16 12:13:50 +0500

    Added part 1

---
+++ Files/Files/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    class Program
+    {
+        static IEnumerable<string> ReadCsv1()
+        {
+            using (var reader = new StreamReader(File.OpenRead("D:\\C_Sharp\\Files\\Files\\airquality.csv")))
+                while (true)
+                {
+                    var str = reader.ReadLine();
+                    if (str == null)
+                    {
+                        reader.Close();
+                        yield break;
+                    }
+
+                    var values = str.Split(',');
+                    foreach(String value in values)
+                    {
+                        if (value == "NA")
+                            yield return null;
+                        else yield return value;
+                    }
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var value in ReadCsv1())
+                Console.WriteLine(value);
+        }
+    }
+}
