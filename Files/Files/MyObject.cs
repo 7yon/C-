fb27d7ce03572057e9c171affddf04d9ddb19041
7yon <simenkovadarya@gmail.com>
2016-11-16 22:55:38 +0500

    Files, part 2

---
+++ Files/Files/MyObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    public class MyObject
+    {
+        public string Name { get; private set; }
+        public int? Ozone { get; private set; }
+        public int? Solar { get; private set; }
+        public double Wind { get; private set; }
+        public int Temp { get; private set; }
+        public int Month { get; private set; }
+        public int Day { get; private set; }
+
+        public MyObject(string Name, int? Ozone, int? Solar, double Wind, int Month, int Day)
+        {
+            this.Name = Name;
+            this.Ozone = Ozone;
+            this.Solar = Solar;
+            this.Wind = Wind;
+            this.Month = Month;
+            this.Day = Day;
+        }
+
+        public MyObject() { Name = "fsf"; }
+    }
+}
--- Files/Files/Program.cs
+++ Files/Files/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +18,6 @@ namespace Files
                     var str = reader.ReadLine();
                     if (str == null)
                     {
-                        reader.Close();
                         yield break;
                     }
 
@@ -26,7 +26,64 @@ namespace Files
                     {
                         if (value == "NA")
                             yield return null;
-                        else yield return value;
+                        else
+                            yield return value;
+                    }
+                }
+        }
+
+        static IEnumerable<T> ReadCsv2<T>() where T : new()
+        {
+            bool firstString = true;
+            string[] fields = null;
+
+            using (var reader = new StreamReader(File.OpenRead("D:\\C_Sharp\\Files\\Files\\airquality.csv")))          
+
+                while (true)
+                {
+                    var str = reader.ReadLine();
+                    if (str == null)
+                    {
+                        yield break;
+                    }
+
+                    var values = str.Split(',');
+
+                    if (firstString)
+                    {
+                        fields = values;
+                        firstString = false;
+                    }
+                    else
+                    {
+                        T myObject = new T();
+                        Type myObjectType = typeof(T);
+
+                        for (int i = 0; i < values.Length; i++)
+                        {                           
+                            PropertyInfo currentInfo = myObjectType.GetProperty(fields[i]);
+
+                            if((values[i] == "NA") && ((fields[i] == "Name") || (fields[i] == "Wind") || (fields[i] == "Temp") || (fields[i] == "Month") || (fields[i] == "Day")))
+                            {
+                                throw new ArgumentException("Поле " + fields[i] + " не может быть null!");
+                            }
+                            else
+                            {
+                                object currentValue = null;
+                                Type currentType = currentInfo.PropertyType;
+
+                                if (values[i] != "NA")
+                                {
+                                    if (currentType == typeof(int) || (currentType == typeof(int?)))
+                                        currentValue = Convert.ToInt32(values[i]);
+                                    if (currentType == typeof(double) || (currentType == typeof(double?)))
+                                        currentValue = Convert.ToDouble(values[i].Replace('.', ','));
+                                }
+                                                               
+                                myObjectType.GetProperty(fields[i]).SetValue(myObject, currentValue);
+                                yield return myObject;
+                            }
+                        }
                     }
                 }
         }
